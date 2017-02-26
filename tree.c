e6f65bcba4000a265f83a01cfb517b4a31b162a9
mnouvw <baixuxyz@gmail.com>
2017-02-26 12:27:27 +0800

    AVL

--- tree.c
+++ tree.c
@@ -76,7 +76,7 @@ Node* insertBST(Node *root, int data){
         if(p->data == data){
             break;
         }
-        if(p->data < data){
+        if(p->data > data){
             if(p->left == NULL){
                 p->left = makeNode(data);
                 break;
@@ -106,7 +106,7 @@ Node* buildBST(int *a, int m, int n){
 Node* search_BST(Node *root, int key){
     Node *p = root;
     while(p != NULL && p->data != key){
-        if(p->data < key){
+        if(p->data > key){
             p = p->left;
         }else{
             p = p->right;
@@ -164,9 +164,130 @@ void union_set(int x, int y){
 
 // ############### Union-Find Set ###############
 
+// ############### AVL Tree ###############
+#define max(x, y) x > y ? x : y
+
+typedef struct AVLNode{
+    int data;
+    int height;
+    struct AVLNode *left;
+    struct AVLNode *right;
+} AVLNode;
+
+AVLNode* make_AVLNode(int data){
+    AVLNode *node = (AVLNode*)malloc(sizeof(AVLNode));
+    node->data = data;
+    node->height = 1;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+AVLNode* ll(AVLNode *root){
+    if(root == NULL || root->left == NULL){
+        return root;
+    }
+    AVLNode *p = root->left;
+    root->left = p->right;
+    p->right = root;
+    root->height--;
+    return p;
+}
+
+AVLNode* rr(AVLNode *root){
+    if(root == NULL || root->right == NULL){
+        return root;
+    }
+    AVLNode *p = root->right;
+    root->right = p->left;
+    p->left = root;
+    return p;
+}
+
+AVLNode* lr(AVLNode *root){
+    root->left = rr(root->left);
+    return ll(root);
+}
+AVLNode* rl(AVLNode *root){
+    root->right = ll(root->right);
+    return rr(root);
+}
+
+AVLNode* rotation(AVLNode *root){
+    if(root == NULL || root->left == NULL || root->right == NULL){
+        return root;
+    }
+}
+
+int height_AVL(AVLNode *root){
+    if(root == NULL){
+        return 0;
+    }
+    return root->height;
+}
+
+AVLNode* insert_AVL(AVLNode *root, int data){
+    if(root == NULL){
+        return make_AVLNode(data);
+    }
+
+    if(root->data == data){
+        return root;
+    }
+    if(data < root->data){
+        root->left = insert_AVL(root->left, data);
+        if(height_AVL(root->left) - height_AVL(root->right) >= 2){
+            if(root->left->data < data){
+                root = ll(root);
+            }else{
+                root = lr(root);
+            }
+        }
+    }else{
+        root->right= insert_AVL(root->right, data);
+        if(height_AVL(root->right) - height_AVL(root->left) >= 2){
+            if(root->right->data < data){
+                root = rl(root);
+            }else{
+                root = rr(root);
+            }
+        }
+    }
+
+    root->height = max(height_AVL(root->left), height_AVL(root->right)) + 1;
+    return root;
+}
+
+AVLNode* build_AVL(int *a, int m, int n){
+    AVLNode *root = NULL;
+    int i = m;
+    for(; i <= n; i++){
+        root = insert_AVL(root, a[i]);
+    }
+    return root;
+}
+
+void in_order_traversal_AVL(AVLNode *root){
+    if(root == NULL){
+        return;
+    }
+    in_order_traversal_AVL(root->left);
+    printf("%d ", root->data);
+    in_order_traversal_AVL(root->right);
+}
+
+
+void order_traversal_AVL(AVLNode *root){
+    in_order_traversal_AVL(root);
+    printf("\n");
+}
+
+
+// ############### AVL Tree ###############
+
 // ############### main ###############
 int main(){
-    printf("order traversal\n");
+    printf("\norder traversal\n---------------\n");
     // order traversal
     Node *root = makeNode(4);
     Node *l = makeNode(1);
@@ -180,7 +301,7 @@ int main(){
     order_traversal(root);
     freeNode(root);
 
-    printf("--------------------\nbst\n");
+    printf("\nbst\n--------------------\n");
 
     // bst
     int n = 10;
@@ -197,7 +318,7 @@ int main(){
     printf("%d %d\n", a[n - 1], k->data);
     freeNode(root);
 
-    printf("--------------------\nunion-find set\n");
+    printf("\nunion-find set\n--------------------\n");
 
     // union-find set
     make_set();
@@ -208,6 +329,11 @@ int main(){
     printf("%d\n", is_same_set(8, 3));
     printf("%d\n", is_same_set(1, 7));
     printf("%d\n", is_same_set(2, 7));
+
+    printf("\nAVL\n--------------------\n");
+
+    AVLNode *avlroot = build_AVL(a, 0, n - 1);
+    order_traversal_AVL(avlroot);
 
     return 0;
 }
